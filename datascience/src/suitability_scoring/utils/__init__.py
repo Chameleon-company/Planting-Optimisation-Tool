047339d141217f9ae4160bb8f4bd0b0f7fefaaa1
Darrin Stephens <dwstephens75@gmail.com>
2026-03-13 13:05:22 +1100

    Change suitability scoring code to utilise SQLAlchemy ORM objects directly

--- datascience/src/suitability_scoring/utils/__init__.py
+++ datascience/src/suitability_scoring/utils/__init__.py
@@ -1,4 +1,5 @@
 from .params import build_species_params_dict, build_rules_dict
 from .config import load_yaml
+from .accessors import get_val
 
-__all__ = ["build_species_params_dict", "build_rules_dict", "load_yaml"]
+__all__ = ["build_species_params_dict", "build_rules_dict", "load_yaml", "get_val"]
